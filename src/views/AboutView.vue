82df3825e7315a27e3f9f3033d2e451ce34fcc36
abcijkxyz <abcijkxyz@163.com>
2022-04-23 22:15:19 +0800

    website

--- src/views/AboutView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import '@material/mwc-icon-button'
-</script>
-
-<template>
-  <mwc-icon-button class="white" icon="arrow_back"></mwc-icon-button>
-</template>
-
-<style lang="less"></style>
--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import DemoView from '@/components/DemoView.vue'
-</script>
-
-<template>
-  <DemoView></DemoView>
-</template>
-
-<style lang="less" scoped></style>
